4143ad083ebddbc4088abee984a18c15f79245cf
aimner <aimner510@gmail.com>
2023-05-30 18:39:34 +0300

    feat: add addTodo api

--- src/app/api/todosApi.ts
+++ src/app/api/todosApi.ts
@@ -1,22 +1,36 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export interface Post {
-  id: number
-  title: string
-  done: boolean
-  text: string
+  id: number;
+  title: string;
+  done: boolean;
+  text: string;
 }
 
 export const todosApi = createApi({
-  reducerPath: 'todosApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://63565c319243cf412f82a6f0.mockapi.io/' }),
+  reducerPath: "todosApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://63565c319243cf412f82a6f0.mockapi.io/" }),
+  tagTypes: ["Todos"],
   endpoints: (build) => ({
-    //              ResultType  QueryArg
-    //                    v       v
-    getTodos: build.query<Post[], null>({query: () => 'todos'}),
+    getTodos: build.query<Post[], null>({
+      query: () => "todos",
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: "Todos" as const, id })),
+              { type: "Todos", id: "LIST" },
+            ]
+          : [{ type: "Todos", id: "LIST" }],
+    }),
+    addTodo: build.mutation<Omit<Post, "id">, Omit<Post, "id">>({
+      query: (body) => ({
+        url: "todos",
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: [{ type: "Todos", id: "LIST" }],
+    }),
   }),
-})
+});
 
-export const { useGetTodosQuery } = todosApi
-
-
+export const { useGetTodosQuery, useAddTodoMutation } = todosApi;
