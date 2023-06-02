f64e2b36bc8d3d405a7f69ea96c45a3cbba3aa42
aimner <aimner510@gmail.com>
2023-06-02 17:55:01 +0300

    feat: add logic to hide or show todo description, input for title

--- src/components/todo/todo.tsx
+++ src/components/todo/todo.tsx
@@ -1,16 +1,24 @@
 import { FC, useState } from "react";
 import { TodoType } from "../../types/todosTypes";
 import { useDeleteTodoMutation, useEditTodoMutation } from "../../app/api/todosApi";
+import classNames from "classnames/bind";
 
 import classes from "./todo.module.scss";
 
 type PropsType = TodoType;
 
+const cx = classNames.bind(classes);
+
 export const Todo: FC<PropsType> = ({ id, text, title }) => {
   const [deleteTodo, {}] = useDeleteTodoMutation();
+
   const [editTodo, {}] = useEditTodoMutation();
+
   const [editMode, setEditMode] = useState(false);
+  const [showAll, setShowAll] = useState(false);
+
   const [todoValue, setTodoValue] = useState(text);
+  const [todoTitle, setTodoTitle] = useState(title);
 
   const onDeleteTodo = async (id: number) => {
     await deleteTodo(id);
@@ -21,22 +29,51 @@ export const Todo: FC<PropsType> = ({ id, text, title }) => {
   };
 
   const saveChangesTodo = async () => {
-    await editTodo({ id, title, done: true, text: todoValue });
-    setEditMode(false)
+    await editTodo({ id, title: todoTitle, done: true, text: todoValue });
+    setEditMode(false);
   };
 
   return (
-    <li className={classes.todo}>
-      <h2 className={classes.todo__title}>{title}</h2>
+    <li
+      className={cx({
+        todo: true,
+        grey: editMode,
+      })}>
       {editMode ? (
-        <textarea
-          value={todoValue}
-          onChange={(e) => setTodoValue(e.target.value)}
-          className={classes.todo__textArea}></textarea>
+        <input
+          value={todoTitle}
+          onChange={(e) => setTodoTitle(e.target.value)}
+          type="text"
+          className={classes.todo__editTitle}
+        />
       ) : (
-        <p className={classes.todo__description}>{text}</p>
+        <h2 className={classes.todo__title}>{title}</h2>
       )}
-      {editMode && <button onClick={saveChangesTodo}>Save changes</button>}
+      {editMode ? (
+        <div className={classes["todo-editTextBlock"]}>
+          <textarea
+            value={todoValue}
+            onChange={(e) => setTodoValue(e.target.value)}
+            className={classes["todo-editTextBlock__textArea"]}></textarea>
+        </div>
+      ) : (
+        <p
+          className={cx({
+            todo__description: true,
+            ["todo__description--hideText"]: !showAll,
+          })}>
+          {text}
+        </p>
+      )}
+      {editMode ? (
+        <button onClick={saveChangesTodo} className={classes.todo_saveChanges}>
+          Save changes
+        </button>
+      ) : (
+        <button className={classes.todo_saveChanges} onClick={(e) => setShowAll((v) => !v)}>
+          {showAll ? 'Hide' : 'Show all'}
+        </button>
+      )}
       <div className={classes["todo-buttons"]}>
         <button
           className={classes["todo-buttons__delete"]}
