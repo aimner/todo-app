772bd48f9aa269c0c5119d178fe921b96b40c895
aimner <aimner510@gmail.com>
2023-06-06 23:20:31 +0300

    feat: add 2 state for sort component

--- src/components/sort/sort.tsx
+++ src/components/sort/sort.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { FC, useState } from "react";
 
-export const Sort = () => {
+import classNames from "classnames/bind";
+
+import classes from "./sort.module.scss";
+
+export const Sort: FC = () => {
+  const [openSelect, setOpenSelect] = useState(false);
+
+  const [activeSortTodoStatus, setActiveSortTodoStatus] = useState("All");
+
   return (
     <section>
       <h2>Sort Params</h2>
+      <div className={classes.lol}>
+        <div>
+          <div>{activeSortTodoStatus}</div>          
+        </div>
+        {openSelect && <div>2</div>}
+      </div>
     </section>
   );
 };
