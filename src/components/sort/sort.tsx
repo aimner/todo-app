9c4034b47cf156d22ea8e7547c7165f66e7aa09b
aimner <aimner510@gmail.com>
2023-06-08 12:26:18 +0300

    feat: add select button

--- src/components/sort/sort.tsx
+++ src/components/sort/sort.tsx
@@ -1,23 +1,35 @@
 import React, { FC, useState } from "react";
-
 import classNames from "classnames/bind";
 
+import arrow from "../../assets/imgs/arrow.png";
+
 import classes from "./sort.module.scss";
+
+const cx = classNames.bind(classes)
 
 export const Sort: FC = () => {
   const [openSelect, setOpenSelect] = useState(false);
 
   const [activeSortTodoStatus, setActiveSortTodoStatus] = useState("All");
 
+  const sortStatusArr = ["All", "Unfinished", "Completed"];
+
   return (
-    <section>
-      <h2>Sort Params</h2>
-      <div className={classes.lol}>
-        <div>
-          <div>{activeSortTodoStatus}</div>          
-        </div>
-        {openSelect && <div>2</div>}
+    <section className={classes.sort}>
+      <div onClick={() => setOpenSelect((v) => !v)} className={classes["sort-select"]}>
+        <div>{activeSortTodoStatus}</div>
+        <img className={cx({
+          ['sort-select__img']: true,
+          ['sort-select__img--rotate']: openSelect
+        })} src={arrow} alt="arrow" />
       </div>
+      {openSelect && (
+        <ul className={classes["sort-select"]}>
+          {sortStatusArr.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+      )}
     </section>
   );
 };
